edd0130c1e79e2f39fa8f75ee6a537df676af0d5
Loodoor <urworld@outlook.fr>
2015-08-25 17:23:01 +0200

    Multiligne, exceptions, fonction (help)

--- arkpov.py
+++ arkpov.py
@@ -29,7 +29,7 @@ class Env(dict):
         self.outer = outer
 
     def __getitem__(self, var):
-        return dict.__getitem__(self, var) if (var in self) else raise_error('KeyError', var + ' doesn\'t exist')
+        return dict.__getitem__(self, var) if (var in self) else raise_error('KeyError', '\'' + var + '\' doesn\'t exist')
 
     def find(self, var):
         if var in self:
@@ -37,7 +37,7 @@ class Env(dict):
         elif self.outer is not None:
             return self.outer.outer.find(var)
         else:
-            raise_error('KeyError', var + ' doesn\'t exist')
+            raise_error('KeyError', '\'' + var + '\' doesn\'t exist')
             return {var: None}
 
 
@@ -49,12 +49,35 @@ class Procedure(object):
         return eval_code(self.body, Env(self.parms, args, self.env))
 
 
+class Buffer:
+    def __init__(self):
+        self.buffer = ""
+
+    def add(self, txt, sep="\n"):
+        self.buffer += str(txt) + sep
+
+    def __str__(self):
+        return self.buffer
+
+
+buffer = Buffer()
+
+
+def print_(*args, end='\r\n', file=sys.stdout, sep=' ', flush=False):
+    for i in args:
+        file.write(str(i) + sep)
+        buffer.add(str(i), sep)
+    file.write(end)
+    if flush:
+        file.flush()
+
+
 def raise_error(err_type, msg):
-    print(err_type, ':', msg)
+    print_(err_type, ':', msg)
 
 
 def return_success(success_type, msg):
-    print(success_type, ':', msg)
+    print_(success_type, ':', msg)
 
 
 def to_py(code):
@@ -89,7 +112,7 @@ def read_from_tokens(tokens):
         tokens.pop(0)  # pop off ')'
         return ast
     elif token == end_token:
-        return raise_error('SyntaxError', 'Unexpected )')
+        return raise_error('SyntaxError', 'Unexpected ' + end_token)
     elif token == comment:
         pass
     else:
@@ -107,6 +130,7 @@ def atom(token):
 
 
 def standard_env():
+    global env
     env = Env()
     env.update(vars(math))  # sin, cos, sqrt, pi, ...
     env.update({
@@ -152,60 +176,145 @@ def standard_env():
 
 global_env = standard_env()
 
+help_lst = [
+    [("quote", "exp"), "Display exp"],
+    [("display",  "var"), "Display the value of var"],
+    [("lambda", "(var...)", "body"), "Create a lambda with parameter(s) var... and body as the code"],
+    [("if", "test", "conseq", "alt"), "If test is true, it will executed conseq. Else, alt will be executed"],
+    [("?", "test"), "If test is true, it will return 1. Else, it will return 0"],
+    [("define", "var", "exp"), "Define var and set its value as exp. If var already exists, it will raise an exception"],
+    [("pyexc", "prgm"), "Execute prgm as a python code"],
+    [("include", "file"), "Include file. If file is not in the standart lib folder, it will search in the current directory for file"],
+    [("set!", "var", "exp"), "Set the value of var as exp. If var doesn't exist, it will raise an exception"]
+]
+
 
 def eval_code(x, env=global_env):
     if isinstance(x, Symbol):  # variable reference
         return env.find(x)[x]
     elif not isinstance(x, List):  # constant literal
         return x
-    elif x[0] == 'quote':  # (quote exp)
-        (_, *exp) = x
-        return ' '.join(exp)
-    elif x[0] == 'display':  # (displar exp)
-        (_, exp) = x
-        return env[exp]
-    elif x[0] == 'lambda':  # (lambda (var...) body)
-        (_, parms, body) = x
-        return Procedure(parms, body, env)
-    elif x[0] == 'if':  # (if test conseq alt)
-        (_, test, conseq, alt) = x
-        exp = conseq if eval_code(test, env) else alt
-        return eval_code(exp, env)
-    elif x[0] == '?':  # (? test) -> 1 | 0
-        (_, test) = x
-        exp = 1 if eval_code(test, env) else 0
-        return exp
-    elif x[0] == 'define':  # (define var exp)
-        (_, var, exp) = x
-        env[var] = eval_code(exp, env)
-    elif x[0] == 'pyexc':  # (pyexc exp)
-        (_, *exp) = x
-        exec(to_py(exp))
-    elif x[0] == 'include':  # (include file)
-        (_, exp) = x
-        env[_](exp)
+    elif x[0] == help_lst[0][0][0]:
+        if len(x) >= len(help_lst[0][0]):
+            (_, *exp) = x
+            return ' '.join(exp)
+        else:
+            return raise_error("ArgumentError", "'" + x[0] + "' need at least " + str(len(help_lst[0][0]) - 1) + " arguments")
+    elif x[0] == help_lst[1][0][0]:
+        if len(x) == len(help_lst[1][0]):
+            (_, exp) = x
+            return env[exp]
+        else:
+            return raise_error("ArgumentError", "'" + x[0] + "' need exactly " + str(len(help_lst[1][0]) - 1) + " arguments")
+    elif x[0] == help_lst[2][0][0]:
+        if len(x) == len(help_lst[2][0]):
+            (_, parms, body) = x
+            return Procedure(parms, body, env)
+        else:
+            return raise_error("ArgumentError", "'" + x[0] + "' need exactly " + str(len(help_lst[2][0]) - 1) + " arguments")
+    elif x[0] == help_lst[3][0][0]:  # (if test conseq alt)
+        if len(x) == len(help_lst[3][0]):
+            (_, test, conseq, alt) = x
+            exp = conseq if eval_code(test, env) else alt
+            return eval_code(exp, env)
+        else:
+            return raise_error("ArgumentError", "'" + x[0] + "' need exactly " + str(len(help_lst[3][0]) - 1) + " arguments")
+    elif x[0] == help_lst[4][0][0]:
+        if len(x) == len(help_lst[4][0]):
+            (_, test) = x
+            exp = 1 if eval_code(test, env) else 0
+            return exp
+        else:
+            return raise_error("ArgumentError", "'" + x[0] + "' need exactly " + str(len(help_lst[4][0]) - 1) + " arguments")
+    elif x[0] == help_lst[5][0][0]:
+        if len(x) == len(help_lst[5][0]):
+            (_, var, exp) = x
+            if var not in env.keys():
+                env[var] = eval_code(exp, env)
+            else:
+                return raise_error("DefineError", "Can't override existing variable. Use set! instead")
+        else:
+            return raise_error("ArgumentError", "'" + x[0] + "' need exactly " + str(len(help_lst[5][0]) - 1) + " arguments")
+    elif x[0] == help_lst[6][0][0]:
+        if len(x) >= len(help_lst[6][0]):
+            (_, *exp) = x
+            exec(to_py(exp))
+        else:
+            return raise_error("ArgumentError", "'" + x[0] + "' need at least " + str(len(help_lst[6][0]) - 1) + " arguments")
+    elif x[0] == help_lst[7][0][0]:
+        if len(x) == len(help_lst[7][0]):
+            (_, exp) = x
+            env[_](exp)
+        else:
+            return raise_error("ArgumentError", "'" + x[0] + "' need exactly " + str(len(help_lst[7][0]) - 1) + " arguments")
+    elif x[0] == help_lst[8][0][0]:
+        if len(x) == len(help_lst[8][0]):
+            (_, var, exp) = x
+            if var in env.keys():
+                env[var] = eval_code(exp, env)
+            else:
+                return raise_error("SetError", "Can't overwrite a non existing variable. Use define instead")
+        else:
+            return raise_error("ArgumentError", "'" + x[0] + "' need exactly " + str(len(help_lst[8][0]) - 1) + " arguments")
+    elif x[0] == 'help':
+        if len(x) == 1:
+            for line in help_lst:
+                print('(', end='')
+                for i in range(len(line[0])):
+                    print(line[0][i], end='')
+                    if i != len(line[0]) - 1:
+                        print('', end=' ')
+                    else:
+                        print(') : ', end='')
+                print(line[1])
+        if len(x) == 2:
+            (_, exp) = x
+            tmp = []
+            for line in help_lst:
+                if line[0][0] == exp:
+                    tmp = line
+                    break
+            if tmp:
+                print('(', end='')
+                for i in range(len(tmp[0])):
+                    print(tmp[0][i], end='')
+                    if i != len(tmp[0]) - 1:
+                        print('', end=' ')
+                    else:
+                        print(') : ', end='')
+                print(tmp[1])
+            else:
+                return raise_error("DocumentationError", "Couldn't find documentation for '" + exp + "'")
     else:  # (proc arg ...)
         proc = eval_code(x[0], env)
         args = [eval_code(arg, env) for arg in x[1:]]
         return proc(*args)
 
 
-def rep(prompt):
-    val = eval_code(parse(input(prompt)))
-    if val is not None:
-        print(schemestr(val))
-    return val
-
-
-def repl():
+def loop():
     std_prompt = language_name + ' > '
     not_eof_prompt = language_name + ' \' '
 
+    prompt = std_prompt
+    code = ""
+
     while True:
-        if True:
-            rep(std_prompt)
-        else:
-            rep(not_eof_prompt)
+        code = input(prompt) if prompt != not_eof_prompt else code + " " + input(prompt)
+
+        if code[-1] != end_token:
+            prompt = not_eof_prompt
+
+            if code in env.keys():
+                prompt = std_prompt
+
+        if prompt == std_prompt or code[-1] == end_token:
+            prompt = std_prompt
+
+            parsed = parse(code)
+            val = eval_code(parsed)
+
+            if val is not None:
+                print_(schemestr(val))
 
 
 def schemestr(exp):
@@ -220,10 +329,10 @@ if __name__ == '__main__':
     try:
         arguments = sys.argv[1:]
         script = arguments[0]
-        with open(script, 'r') as code:
-            print(eval_code(parse(code.read())))
+        with open(script, 'r') as code_:
+            print(eval_code(parse(code_.read())))
             return_success('ReadingSuccess', "File '" + script + "' successfully loaded and read")
     except IndexError:
         pass
 
-    repl()
+    loop()
